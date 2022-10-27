9d8516495e63e78d6c334a79c8fdfb223ac55971
juho pohjonen <juhopohjonen237@gmail.com>
2022-10-27 15:27:40 +0300

    added new language (germany)

--- app.py
+++ app.py
@@ -23,8 +23,9 @@ app.mount('/static', StaticFiles(directory='static'), name='static')
 import random
 
 default_wiki_code = 'fi'
-other_accepted_wikis = ['en', 'sv']
-code_definitions = [{ 'code': 'en', 'name': 'Englanti' }, { 'code': 'fi', 'name': 'Suomi' }, { 'code': 'sv', 'name': 'Ruotsi' }]
+other_accepted_wikis = ['en', 'sv', 'de']
+code_definitions = [{ 'code': 'en', 'name': 'Englanti' }, { 'code': 'fi', 'name': 'Suomi' }, { 'code': 'sv', 'name': 'Ruotsi' },
+{ 'code': 'de', 'name': 'Saksa' }]
 
 
 default_wiki = wpa.Wikipedia(default_wiki_code)
